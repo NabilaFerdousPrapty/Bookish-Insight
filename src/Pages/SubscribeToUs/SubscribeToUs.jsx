a205f7469f45b045837e7a896a8c29b1aeedc055
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-03-28 21:58:33 +0600

    done

--- src/Pages/SubscribeToUs/SubscribeToUs.jsx
+++ src/Pages/SubscribeToUs/SubscribeToUs.jsx
@@ -1,7 +1,7 @@
-import '../../assets/style.css'
+import "../../assets/style.css";
 const SubscribeToUs = () => {
   return (
-    <div className='mx-1'>
+    <div className="mx-1">
       <div className="p-6 py-12 bg-[#23BE0A] rounded-3xl">
         <div className="container mx-auto">
           <div className="flex flex-col lg:flex-row items-center justify-between">
@@ -33,16 +33,16 @@ const SubscribeToUs = () => {
               Choose your best plan
             </h2>
           </div>
-          <div className="flex flex-wrap items-stretch -mx-4">
-            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+          <div className="flex flex-wrap items-stretch -mx-4 ">
+            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 hover:scale-95">
               <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-50">
                 <div className="space-y-2">
                   <h4 className="text-2xl font-bold">Beginner</h4>
                   <span className="text-6xl font-bold">Free</span>
                 </div>
                 <p className="mt-3 leading-relaxed text-gray-600">
-        Start your journey with the Beginner plan, absolutely free.
-      </p>
+                  Start your journey with the Beginner plan, absolutely free.
+                </p>
                 <ul className="flex-1 mb-6 text-gray-600">
                   <li className="flex mb-2 space-x-2">
                     <svg
@@ -57,9 +57,7 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>
-                    Access to basic book reviews and ratings
-                    </span>
+                    <span>Access to basic book reviews and ratings</span>
                   </li>
                   <li className="flex mb-2 space-x-2">
                     <svg
@@ -115,7 +113,7 @@ const SubscribeToUs = () => {
                 </button>
               </div>
             </div>
-            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 hover:scale-95">
               <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-[#23BE0A] text-gray-50">
                 <div className="space-y-2">
                   <h4 className="text-2xl font-bold">Pro</h4>
@@ -125,7 +123,7 @@ const SubscribeToUs = () => {
                   </span>
                 </div>
                 <p className="leading-relaxed">
-                Access to premium book reviews from renowned critics
+                  Access to premium book reviews from renowned critics
                 </p>
                 <ul className="flex-1 space-y-2">
                   <li className="flex items-center space-x-2">
@@ -141,7 +139,9 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Unlimited access to curated book lists and recommendations</span>
+                    <span>
+                      Unlimited access to curated book lists and recommendations
+                    </span>
                   </li>
                   <li className="flex items-center space-x-2">
                     <svg
@@ -156,7 +156,9 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Advanced search filters by genre, author, rating, etc.</span>
+                    <span>
+                      Advanced search filters by genre, author, rating, etc.
+                    </span>
                   </li>
                   <li className="flex items-center space-x-2">
                     <svg
@@ -171,7 +173,9 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Personalized reading suggestions based on reading history</span>
+                    <span>
+                      Personalized reading suggestions based on reading history
+                    </span>
                   </li>
                   <li className="flex items-center space-x-2">
                     <svg
@@ -186,7 +190,9 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Enhanced user profile with customizable preferences</span>
+                    <span>
+                      Enhanced user profile with customizable preferences
+                    </span>
                   </li>
                   <li className="flex items-center space-x-2">
                     <svg
@@ -201,19 +207,20 @@ const SubscribeToUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Ad-free experience for a seamless reading environment</span>
+                    <span>
+                      Ad-free experience for a seamless reading environment
+                    </span>
                   </li>
                 </ul>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
+                <button
+                  
                   className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-gray-100 text-[#23BE0A]"
                 >
                   Get Started
-                </a>
+                </button>
               </div>
             </div>
-            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 hover:scale-95">
               <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-50">
                 <div className="space-y-2">
                   <h4 className="text-2xl font-bold">Team</h4>
@@ -223,8 +230,8 @@ const SubscribeToUs = () => {
                   </span>
                 </div>
                 <p className="leading-relaxed text-gray-600">
-                All Pro plan features, plus:
-Collaborative book clubs with friends or colleagues
+                  All Pro plan features, plus: Collaborative book clubs with
+                  friends or colleagues
                 </p>
                 <ul className="space-y-2 text-gray-600">
                   <li className="flex items-start space-x-2">
@@ -270,7 +277,9 @@ Collaborative book clubs with friends or colleagues
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Advanced analytics on reading habits and preferences</span>
+                    <span>
+                      Advanced analytics on reading habits and preferences
+                    </span>
                   </li>
                   <li className="flex items-start space-x-2">
                     <svg
@@ -285,7 +294,9 @@ Collaborative book clubs with friends or colleagues
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Dedicated account manager for personalized assistance</span>
+                    <span>
+                      Dedicated account manager for personalized assistance
+                    </span>
                   </li>
                   <li className="flex items-start space-x-2">
                     <svg
@@ -300,7 +311,10 @@ Collaborative book clubs with friends or colleagues
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Custom branding and integration options for book clubs or organizations</span>
+                    <span>
+                      Custom branding and integration options for book clubs or
+                      organizations
+                    </span>
                   </li>
                   <li className="flex items-start space-x-2">
                     <svg
@@ -315,22 +329,26 @@ Collaborative book clubs with friends or colleagues
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Priority access to exclusive author interviews and events</span>
+                    <span>
+                      Priority access to exclusive author interviews and events
+                    </span>
                   </li>
                 </ul>
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
+                <button
+                  
                   className="inline-block w-full px-5 py-3 font-semibold tracking-wider text-center rounded bg-[#23BE0A] text-gray-50"
                 >
                   Get Started
-                </a>
+                </button>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <h2 className="text-4xl text-center playfair-display-Bold"> Last Month Analysis</h2>
+      <h2 className="text-4xl text-center playfair-display-Bold">
+        {" "}
+        Last Month Analysis
+      </h2>
       <section className="p-6 my-6 bg-gray-100 text-gray-800">
         <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-4">
           <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
@@ -409,37 +427,54 @@ Collaborative book clubs with friends or colleagues
           </div>
         </div>
       </section>
-    
-<div className="text-center ">
-<div className="mockup-phone  xs:w-auto w-[83%]">
-  <div className="camera"></div> 
-  <div className="display">
-    <div className="artboard artboard-demo  xs:items-center items-start phone-1 ">
-    <div className="flex flex-col sm:text-center text-left ">
-			<p className="mb-1 text-sm font-medium  uppercase text-[#23BE0A]">Discover New Reads</p>
-			<h1 className="py-2 text-xl font-medium  title-font">Bookish<br className="sm:hidden" /> Insight:<br className="sm:hidden" /> Dive into<br className="sm:hidden" /> Captivating<br className="sm:hidden" /> Stories</h1>
-		</div>
-        <button className="inline-flex justify-center items-center my-4 px-6 py-3 rounded-lg bg-[#23BE0A] text-gray-50">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-current w-7 h-7 text-gray-50">
-					<path d="M 5.4160156 2.328125 L 12.935547 10.158203 C 13.132547 10.363203 13.45925 10.363203 13.65625 10.158203 L 15.179688 8.5742188 C 15.405688 8.3392188 15.354312 7.956875 15.070312 7.796875 C 11.137313 5.571875 6.2620156 2.811125 5.4160156 2.328125 z M 3.140625 2.8476562 C 3.055625 3.0456562 3 3.2629063 3 3.5039062 L 3 20.591797 C 3 20.788797 3.044375 20.970625 3.109375 21.140625 L 11.576172 12.324219 C 11.762172 12.131219 11.762172 11.826813 11.576172 11.632812 L 3.140625 2.8476562 z M 17.443359 9.2578125 C 17.335484 9.2729375 17.233297 9.32375 17.154297 9.40625 L 15.015625 11.632812 C 14.829625 11.825812 14.829625 12.130219 15.015625 12.324219 L 17.134766 14.529297 C 17.292766 14.694297 17.546141 14.729188 17.744141 14.617188 C 19.227141 13.777188 20.226563 13.212891 20.226562 13.212891 C 20.725562 12.909891 21.007 12.443547 21 11.935547 C 20.992 11.439547 20.702609 10.981938 20.224609 10.710938 C 20.163609 10.676937 19.187672 10.124359 17.763672 9.3183594 C 17.664172 9.2623594 17.551234 9.2426875 17.443359 9.2578125 z M 13.296875 13.644531 C 13.165875 13.644531 13.034047 13.696328 12.935547 13.798828 L 5.4746094 21.566406 C 6.7566094 20.837406 11.328781 18.249578 15.050781 16.142578 C 15.334781 15.981578 15.386156 15.599281 15.160156 15.363281 L 13.65625 13.798828 C 13.55775 13.696328 13.427875 13.644531 13.296875 13.644531 z"></path>
-				</svg>
-				<span className="flex flex-col items-start ml-4 leading-none">
-					<span className="mb-1 text-xs">GET IT ON</span>
-					<span className="font-semibold title-font">Google Play</span>
-				</span>
-			</button>
-			<button className="inline-flex justify-center items-center px-5 py-3 rounded-lg bg-[#23BE0A] text-gray-50">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="fill-current w-7 h-7 text-gray-50">
-					<path d="M 44.527344 34.75 C 43.449219 37.144531 42.929688 38.214844 41.542969 40.328125 C 39.601563 43.28125 36.863281 46.96875 33.480469 46.992188 C 30.46875 47.019531 29.691406 45.027344 25.601563 45.0625 C 21.515625 45.082031 20.664063 47.03125 17.648438 47 C 14.261719 46.96875 11.671875 43.648438 9.730469 40.699219 C 4.300781 32.429688 3.726563 22.734375 7.082031 17.578125 C 9.457031 13.921875 13.210938 11.773438 16.738281 11.773438 C 20.332031 11.773438 22.589844 13.746094 25.558594 13.746094 C 28.441406 13.746094 30.195313 11.769531 34.351563 11.769531 C 37.492188 11.769531 40.8125 13.480469 43.1875 16.433594 C 35.421875 20.691406 36.683594 31.78125 44.527344 34.75 Z M 31.195313 8.46875 C 32.707031 6.527344 33.855469 3.789063 33.4375 1 C 30.972656 1.167969 28.089844 2.742188 26.40625 4.78125 C 24.878906 6.640625 23.613281 9.398438 24.105469 12.066406 C 26.796875 12.152344 29.582031 10.546875 31.195313 8.46875 Z"></path>
-				</svg>
-				<span className="flex flex-col items-start ml-4 leading-none ">
-					<span className="mb-1 text-xs">Download on the</span>
-					<span className="font-semibold title-font">App Store</span>
-				</span>
-			</button></div>
-  </div>
-</div>
-</div>
+
+      <div className="text-center ">
+        <div className="mockup-phone  xs:w-auto w-[83%]">
+          <div className="camera"></div>
+          <div className="display">
+            <div className="artboard artboard-demo  xs:items-center items-start phone-1 ">
+              <div className="flex flex-col sm:text-center text-left ">
+                <p className="mb-1 text-sm font-medium  uppercase text-[#23BE0A]">
+                  Discover New Reads
+                </p>
+                <h1 className="py-2 text-xl font-medium  title-font">
+                  Bookish
+                  <br className="sm:hidden" /> Insight:
+                  <br className="sm:hidden" /> Dive into
+                  <br className="sm:hidden" /> Captivating
+                  <br className="sm:hidden" /> Stories
+                </h1>
+              </div>
+              <button className="inline-flex justify-center items-center my-4 px-6 py-3 rounded-lg bg-[#23BE0A] text-gray-50">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="fill-current w-7 h-7 text-gray-50"
+                >
+                  <path d="M 5.4160156 2.328125 L 12.935547 10.158203 C 13.132547 10.363203 13.45925 10.363203 13.65625 10.158203 L 15.179688 8.5742188 C 15.405688 8.3392188 15.354312 7.956875 15.070312 7.796875 C 11.137313 5.571875 6.2620156 2.811125 5.4160156 2.328125 z M 3.140625 2.8476562 C 3.055625 3.0456562 3 3.2629063 3 3.5039062 L 3 20.591797 C 3 20.788797 3.044375 20.970625 3.109375 21.140625 L 11.576172 12.324219 C 11.762172 12.131219 11.762172 11.826813 11.576172 11.632812 L 3.140625 2.8476562 z M 17.443359 9.2578125 C 17.335484 9.2729375 17.233297 9.32375 17.154297 9.40625 L 15.015625 11.632812 C 14.829625 11.825812 14.829625 12.130219 15.015625 12.324219 L 17.134766 14.529297 C 17.292766 14.694297 17.546141 14.729188 17.744141 14.617188 C 19.227141 13.777188 20.226563 13.212891 20.226562 13.212891 C 20.725562 12.909891 21.007 12.443547 21 11.935547 C 20.992 11.439547 20.702609 10.981938 20.224609 10.710938 C 20.163609 10.676937 19.187672 10.124359 17.763672 9.3183594 C 17.664172 9.2623594 17.551234 9.2426875 17.443359 9.2578125 z M 13.296875 13.644531 C 13.165875 13.644531 13.034047 13.696328 12.935547 13.798828 L 5.4746094 21.566406 C 6.7566094 20.837406 11.328781 18.249578 15.050781 16.142578 C 15.334781 15.981578 15.386156 15.599281 15.160156 15.363281 L 13.65625 13.798828 C 13.55775 13.696328 13.427875 13.644531 13.296875 13.644531 z"></path>
+                </svg>
+                <span className="flex flex-col items-start ml-4 leading-none">
+                  <span className="mb-1 text-xs">GET IT ON</span>
+                  <span className="font-semibold title-font">Google Play</span>
+                </span>
+              </button>
+              <button className="inline-flex justify-center items-center px-5 py-3 rounded-lg bg-[#23BE0A] text-gray-50">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 50 50"
+                  className="fill-current w-7 h-7 text-gray-50"
+                >
+                  <path d="M 44.527344 34.75 C 43.449219 37.144531 42.929688 38.214844 41.542969 40.328125 C 39.601563 43.28125 36.863281 46.96875 33.480469 46.992188 C 30.46875 47.019531 29.691406 45.027344 25.601563 45.0625 C 21.515625 45.082031 20.664063 47.03125 17.648438 47 C 14.261719 46.96875 11.671875 43.648438 9.730469 40.699219 C 4.300781 32.429688 3.726563 22.734375 7.082031 17.578125 C 9.457031 13.921875 13.210938 11.773438 16.738281 11.773438 C 20.332031 11.773438 22.589844 13.746094 25.558594 13.746094 C 28.441406 13.746094 30.195313 11.769531 34.351563 11.769531 C 37.492188 11.769531 40.8125 13.480469 43.1875 16.433594 C 35.421875 20.691406 36.683594 31.78125 44.527344 34.75 Z M 31.195313 8.46875 C 32.707031 6.527344 33.855469 3.789063 33.4375 1 C 30.972656 1.167969 28.089844 2.742188 26.40625 4.78125 C 24.878906 6.640625 23.613281 9.398438 24.105469 12.066406 C 26.796875 12.152344 29.582031 10.546875 31.195313 8.46875 Z"></path>
+                </svg>
+                <span className="flex flex-col items-start ml-4 leading-none ">
+                  <span className="mb-1 text-xs">Download on the</span>
+                  <span className="font-semibold title-font">App Store</span>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
